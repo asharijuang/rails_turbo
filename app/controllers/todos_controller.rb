2691b29127df7e16035e5dbd76d1b2e2ea1551b6
asharijuang <asharijuang@me.com>
2023-04-27 21:25:51 +0700

    turbo frame, stream

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -25,9 +25,16 @@ class TodosController < ApplicationController
 
     respond_to do |format|
       if @todo.save
+        format.turbo_stream
         format.html { redirect_to todo_url(@todo), notice: "Todo was successfully created." }
         format.json { render :show, status: :created, location: @todo }
       else
+        format.turbo_stream { 
+          render turbo_stream: turbo_stream.replace(
+            "#{helpers.dom_id(@todo)}_form", 
+            partial: "form", 
+            locals: { todo: @todo }) 
+        }
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @todo.errors, status: :unprocessable_entity }
       end
@@ -38,9 +45,16 @@ class TodosController < ApplicationController
   def update
     respond_to do |format|
       if @todo.update(todo_params)
+        format.turbo_stream 
         format.html { redirect_to todo_url(@todo), notice: "Todo was successfully updated." }
         format.json { render :show, status: :ok, location: @todo }
       else
+        format.turbo_stream { 
+          render turbo_stream: turbo_stream.replace(
+            "#{helpers.dom_id(@todo)}_form", 
+            partial: "form", 
+            locals: { todo: @todo }) 
+        }
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @todo.errors, status: :unprocessable_entity }
       end
